0245a853f6c95e1334d416297d7fd519885c8683
zhoujinyong1 <51100742+zhoujinyong1@users.noreply.github.com>
2021-05-31 10:43:18 +0800

    Add files via upload

---
+++ zjy（iOS端计算器）/zjy/Common.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Smobiler.Core.Web;
+
+namespace zjy
+{
+    public class Common
+    {
+        private static double f = 0.0;
+        private static string shuzi = "";
+        private static string result = "";
+        public static double dou = 0.0;
+        public static string Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+
+        public static string Shuzi
+        {
+            get { return shuzi; }
+            set { shuzi = value; }
+        }
+
+        public static double F
+        {
+            get { return f; }
+            set { f = value; }
+        }
+
+
+
+    }
+}
